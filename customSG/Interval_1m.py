beca4d5716af160b42ae66d811c649087c6ce44e
saurabh.gupta <saurabh.gupta@ibm.com>
2020-03-25 17:21:43 -0500

    1min function

--- customSG/Interval_1m.py
+++ customSG/Interval_1m.py
@@ -18,15 +18,15 @@ PACKAGE_URL = 'git+https://github.com/sauraba/monitorfunction.git@master'
 
 class Interval_1min (BaseTransformer):
 
-    def __init__(self, inputs_items, output_item):
+    def __init__(self, inputs_items, output_items):
 
         self.inputs_items = inputs_items
-        self.output_item = output_item
+        self.output_items = output_items
         super().__init__()
     def execute(self, df):
         df=pd.dataframe(columns=[self.inputs_items[0],self.inputs_items[1],self.inputs_items[2]])
         #df=pd.dataframe(output_items)
-        df[self.output_item[0]] = df.groupby(['siteId', pd.to_datetime(df['Intervaldttm'].str[:16])])['HouseAir'].mean()
+        df[self.output_items[0]] = df.groupby(['siteId', pd.to_datetime(df['Intervaldttm'].str[:16])])['HouseAir'].mean()
         return df
 
     @classmethod
@@ -37,7 +37,7 @@ class Interval_1min (BaseTransformer):
                 name = 'input_items',
                 datatype=float,
                 description = "Data items adjust",
-                output_item = 'output_item',
+                output_item = 'output_items',
                 is_output_datatype_derived = True)
                       )
 
